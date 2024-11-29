dca45de1ee3b93dedee1ba2ad883a5a4460005de
teppolap <teppo.lappalainen28@gmail.com>
2024-11-29 11:59:03 +0200

    .

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -3,11 +3,11 @@ import { headers } from 'next/headers';
 import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
 
 export async function POST(req) {
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const SIGNING_SECRET = process.env.SIGNING_SECRET;
 
-  if (!WEBHOOK_SECRET) {
+  if (!SIGNING_SECRET) {
     throw new Error(
-      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+      'Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local'
     );
   }
 
@@ -29,7 +29,7 @@ export async function POST(req) {
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+  const wh = new Webhook(SIGNING_SECRET);
 
   let evt;
 
