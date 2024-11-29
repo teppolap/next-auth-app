ca527cd00eff52e73a4f3fe2bd0f7c0d539304d9
teppolap <teppo.lappalainen28@gmail.com>
2024-11-29 11:47:46 +0200

    Add mongodb and mongoose to save user data from clerk

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,61 +1,93 @@
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
 
 export async function POST(req) {
-  const SIGNING_SECRET = process.env.SIGNING_SECRET
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-  if (!SIGNING_SECRET) {
-    throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+    );
   }
 
-  // Create new Svix instance with secret
-  const wh = new Webhook(SIGNING_SECRET)
-
-  // Get headers
-  const headerPayload = await headers()
-  const svix_id = headerPayload.get('svix-id')
-  const svix_timestamp = headerPayload.get('svix-timestamp')
-  const svix_signature = headerPayload.get('svix-signature')
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+  const svix_signature = headerPayload.get('svix-signature');
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error: Missing Svix headers', {
+    return new Response('Error occured -- no svix headers', {
       status: 400,
-    })
+    });
   }
 
-  // Get body
-  const payload = await req.json()
-  const body = JSON.stringify(payload)
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
-  let evt
+  // Create a new Svix instance secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
 
-  // Verify payload with headers
+  let evt;
+
+  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       'svix-id': svix_id,
       'svix-timestamp': svix_timestamp,
       'svix-signature': svix_signature,
-    })
+    });
   } catch (err) {
-    console.error('Error: Could not verify webhook:', err)
-    return new Response('Error: Verification error', {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
       status: 400,
-    })
+    });
   }
 
-  const { id } = evt.data
-  const eventType = evt.type
-  console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-  console.log('Webhook payload:', body)
+  const { id } = evt?.data;
+  const eventType = evt?.type;
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log('Webhook body:', body);
 
-  if (eventType === 'user.created') {
-    console.log('User created');
+  if (eventType === 'user.created' || eventType === 'user.updated') {
+    const { id, first_name, last_name, image_url, email_addresses, username } =
+      evt?.data;
+    try {
+      await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username
+      );
+      return new Response('User is created or updated', {
+        status: 200,
+      });
+    } catch (error) {
+      console.log('Error creating or updating user:', error);
+      return new Response('Error occured', {
+        status: 400,
+      });
+    }
+  }
+  if (eventType === 'user.deleted') {
+    const { id } = evt?.data;
+    try {
+      await deleteUser(id);
+      return new Response('User is deleted', {
+        status: 200,
+      });
+    } catch (error) {
+      console.log('Error deleting user:', error);
+      return new Response('Error occured', {
+        status: 400,
+      });
+    }
   }
 
-  if (eventType === 'user.updated') {
-    console.log('User updated');
-  }
-
-  return new Response('Webhook received', { status: 200 })
+  return new Response('', { status: 200 });
 }
